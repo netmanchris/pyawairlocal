4aeef58f96b9474899d84ee454cc25c00a55f61c
Chris Young <chris_young@me.com>
2020-10-18 16:27:33 -0400

    Added basic_usage to drive development and initial client and data with functions to get device configuration and latest data.

--- data.py
+++ data.py
@@ -12,10 +12,11 @@ def get_dev_config(awairclient, data=None):
 
 def get_dev_data(awairclient, data=None):
     '''
-
+    Function to fetch latest sensor readings from support Awair device with Local REST
+    API enabled
     :param awairclient:
     :param data:
-    :return:
+    :return: JSON object representing sensor readings
     '''
     path = 'air-data/latest'
     response = awairclient.get(path)
